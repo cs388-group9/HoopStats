838310e3251a0f1ae45b746f4472c7e99c004b57
arsalanKhawar <ak78@njit.edu>
2024-04-06 18:35:38 -0400

    updated gradle files for firebase

--- HoopStats/app/build.gradle.kts
+++ HoopStats/app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
