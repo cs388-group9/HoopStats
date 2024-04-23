abeddd90f1c7b1359a6828828ed0ee9115afa5d7
arsalanKhawar <ak78@njit.edu>
2024-04-23 16:20:36 -0400

    increment/decrement stats work now

--- HoopStats/app/src/main/java/com/example/hoopstats/models/Player.kt
+++ HoopStats/app/src/main/java/com/example/hoopstats/models/Player.kt
@@ -4,15 +4,17 @@ import java.io.Serializable
 
 data class Player(
     val playerName: String = "",
+    val teamId: String = "",
     var rebounds: Int = 0,
     var assists: Int = 0,
     var twoPointers: Int = 0,
     var threePointers: Int = 0,
-    var freeThrows: Int = 0
+    var freeThrows: Int = 0,
+    var playerId: String = "" // Added playerId field
 ) : Serializable {
     val totalPoints: Int
         get() = twoPointers * 2 + threePointers * 3 + freeThrows
 
     // Default constructor
-    constructor() : this("", 0, 0, 0, 0, 0)
+    constructor() : this("", "", 0, 0, 0, 0, 0, "")
 }
