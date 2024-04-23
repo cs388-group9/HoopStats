c291d1521bb706552bdf09f8c60990f0121cabf2
Jay Santamaria <js288@njit.edu>
2024-04-23 00:19:17 -0400

    Page for Track Stats

--- HoopStats/app/build.gradle.kts
+++ HoopStats/app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
+    implementation ("com.android.volley:volley:1.2.1")
+
 }
